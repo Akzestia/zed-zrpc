5b05cbb540ead161a61a1b8954ea867a9ef3eece
アクゼスティア <akzestia@gmail.com>
2025-04-02 18:02:26 +0200

    Updated highlights.scm

--- languages/zrpc/highlights.scm
+++ languages/zrpc/highlights.scm
@@ -1,52 +1,16 @@
 
-(scheme_definition
-  "scheme" @keyword)
 
-(scheme_name) @type.definition
+[
+    "import"
+    "scheme"
+    "route"
+    "request"
+    "response"
+] @keyword
+
+(type) @type
 
 (field_name) @variable
+(identifier) @variable
 
-(scheme_type) @type
-
-; List of working @ in Zed
-; Capture	Description
-; @attribute	Captures attributes
-; @boolean	Captures boolean values
-; @comment	Captures comments
-; @comment.doc	Captures documentation comments
-; @constant	Captures constants
-; @constructor	Captures constructors
-; @embedded	Captures embedded content
-; @emphasis	Captures emphasized text
-; @emphasis.strong	Captures strongly emphasized text
-; @enum	Captures enumerations
-; @function	Captures functions
-; @hint	Captures hints
-; @keyword	Captures keywords
-; @label	Captures labels
-; @link_text	Captures link text
-; @link_uri	Captures link URIs
-; @number	Captures numeric values
-; @operator	Captures operators
-; @predictive	Captures predictive text
-; @preproc	Captures preprocessor directives
-; @primary	Captures primary elements
-; @property	Captures properties
-; @punctuation	Captures punctuation
-; @punctuation.bracket	Captures brackets
-; @punctuation.delimiter	Captures delimiters
-; @punctuation.list_marker	Captures list markers
-; @punctuation.special	Captures special punctuation
-; @string	Captures string literals
-; @string.escape	Captures escaped characters in strings
-; @string.regex	Captures regular expressions
-; @string.special	Captures special strings
-; @string.special.symbol	Captures special symbols
-; @tag	Captures tags
-; @tag.doctype	Captures doctypes (e.g., in HTML)
-; @text.literal	Captures literal text
-; @title	Captures titles
-; @type	Captures types
-; @variable	Captures variables
-; @variable.special	Captures special variables
-; @variant	Captures variants
+(string_literal) @string
